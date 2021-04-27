1e61427390b40388ec68536cc716011b84873801
Divyashree BS <divya.bs1996@gmail.com>
2021-04-27 15:17:26 +0530

    Adding the src exercise file

--- src/exercises.py
+++ src/exercises.py
@@ -2,10 +2,16 @@ def reverse_list(input_list):
     """
     Reverses order of elements in list input_list.
     """
-    return None
+    return input_list[::-1]
 
 def count_digits(number):
     """
     Return count of digits
     """
-    return None
+    count = 0
+    for i in str(number):
+        if i.isdigit():
+            count+=1
+        else:
+            continue
+    return count
